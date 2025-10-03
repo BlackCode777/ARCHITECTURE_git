a1091cd4cb7ef7461da0ba87398e0924075eebd9
Anderson Martins <BlackCode77.1980@gmail.com>
2025-10-03 07:54:27 -0300

    00 _ Iniciando estudo das arquiteturas de software.

---
+++ padraocriacional/src/main/java/com/blackcode/padraocriacional/PadraocriacionalApplication.java
@@ -0,0 +1,13 @@
+package com.blackcode.padraocriacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadraocriacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadraocriacionalApplication.class, args);
+	}
+
+}
