2e76e1e3e5421b3a4ab094566f979642416f8c1e
Anderson Martins <BlackCode77.1980@gmail.com>
2025-10-07 07:51:05 -0300

    03 - Estudo da arquitetura FACADE - limpeza.

---
+++ arquitecturefacade/src/test/java/com/blackcode/arquitecturefacade/ArquitecturefacadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackcode.arquitecturefacade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquitecturefacadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
