722350c0c7ee27c4feca0165f15e231f4088962c
Anderson Martins <BlackCode77.1980@gmail.com>
2025-10-09 06:08:04 -0300

    04 - Estudo Arquitetura Composite.

---
+++ arquitecturecomposite/src/main/java/com/blackcode/arquitecturecomposite/ArquitecturecompositeApplication.java
@@ -0,0 +1,13 @@
+package com.blackcode.arquitecturecomposite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturecompositeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquitecturecompositeApplication.class, args);
+	}
+
+}
