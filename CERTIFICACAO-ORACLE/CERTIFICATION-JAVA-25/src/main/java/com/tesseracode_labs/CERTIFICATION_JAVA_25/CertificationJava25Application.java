5933a9c8d7162d440614486d2bb7f46be163c5b9
Anderson Martins <BlackCode77.1980@gmail.com>
2026-09-12 08:02:00 -0300

    Iniciando estudos para certificação - java 25.

---
+++ CERTIFICACAO-ORACLE/CERTIFICATION-JAVA-25/src/main/java/com/tesseracode_labs/CERTIFICATION_JAVA_25/CertificationJava25Application.java
@@ -0,0 +1,13 @@
+package com.tesseracode_labs.CERTIFICATION_JAVA_25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationJava25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationJava25Application.class, args);
+	}
+
+}
---
+++ CERTIFICACAO-ORACLE/CERTIFICATION-JAVA-25/src/test/java/com/tesseracode_labs/CERTIFICATION_JAVA_25/CertificationJava25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesseracode_labs.CERTIFICATION_JAVA_25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationJava25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
